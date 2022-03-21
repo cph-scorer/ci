f574bfbc1e7e7eeac5eeebd11d4c67000fb558fb
Corentin Verquin <corentin.verquin@gmail.com>
2022-03-21 17:03:27 +0100

    Update register.sh

--- .github/utils/deploy-client/register.sh
+++ .github/utils/deploy-client/register.sh
@@ -4,6 +4,7 @@ export service=${GITHUB_REPOSITORY#*/}
 export url=$(echo https://${BUCKET}.s3.eu-west-3.amazonaws.com/)
 export file=$(echo ${DIRECTORY}/${ARTIFACT})
 
-envsubst <<<$(curl https://raw.githubusercontent.com/cph-scorer/ci/main/.github/utils/deploy-client/payload.json) > payload.json
+curl -s https://raw.githubusercontent.com/cph-scorer/ci/main/.github/utils/deploy-client/payload.json -o payload.json
+envsubst < payload.json | tee payload.json > /dev/null 
 
 curl --data @payload.json -X PATCH localhost:5000/services
